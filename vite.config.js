eb7feda0aee00af54c055eec5aa8d0c2f4e6e238
SaneFungus <63272836+SaneFungus@users.noreply.github.com>
2025-05-03 18:48:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,9 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
-    svelte(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Fiszki do nauki tekstu',
-        short_name: 'Fiszki-Tekst',
-        description: 'Aplikacja do nauki tekstu dla aktorów',
-        theme_color: '#3A5E85',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    svelte()
   ],
-  base: '/scenariusz-fiszki/', // Zmień na nazwę Twojego repozytorium!
+  base: '/'
 });
